4381fd2459091e929a8b9e3c26e02d9331d4af80
mostafa <mostafa18051b@yahoo.com>
2020-11-24 10:45:47 +0330

    Check the type

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,55 +15,65 @@ public class Main   {
 
     public static void main(String[] args) {
 
-
+            boolean Flage=false;
             Properties  Input =new Properties();
         for(int CounterA=0;CounterA<6;CounterA +=2)
-        {
+             {
+                 Scanner Scan = new Scanner(System.in);
+                 String InputA;
+                 String Inputc;
+                 int InputB = 0;
+                 while(true)
+                     {
+                         System.out.println("Input String: ");
+                         InputA = Scan.next();
+                         /*Check the input length*/
+                        if(InputA.length()<=10){break;}
+                            else
+                                {System.out.print("re");Flage=false;}
+                     }
 
-            Scanner Scan = new Scanner(System.in);
-            String InputA;
-            int InputB;
-            while(true) {
-                System.out.println("Input String: ");
-                  InputA = Scan.next();
-                if(InputA.length()<=5){break;}else {System.out.print("re");}
-            }
-            while(true) {
-                System.out.println("Input  Integer: ");
-                InputB = Scan.nextInt( 3);
-
-                if(InputB<= 999 && InputB>=0){break;}else {System.out.print("re");}
-            }
-
+                 while(true)
+                     {
+                         System.out.println("Input  Integer: ");
+                          try
+                             {
+                                 InputB = Scan.nextInt(  );
+                                 Flage=false;
+                             }
+                             catch (Exception e)
+                             {
+                                 /*Check the input type*/
+                                 Flage=true; System.out.println("press enter");
+                                 Inputc = Scan.next();
+                             }
+                             /*Check the input length*/
+                             if(InputB<= 999 && InputB>=0 && (Flage==false))
+                                 {break;}
+                             else
+                                 {System.out.print("re");}
+                     }
+            /* Insert input in Properties */
 
             Input.put(String.valueOf(CounterA),InputA);
             Input.put(String.valueOf(CounterA+1),String.valueOf(InputB));
 
         }
 
-            try(FileOutputStream Output = new FileOutputStream("save. Properties"); )
+        try(FileOutputStream Output = new FileOutputStream("save. Properties"); )
             {
-
+                /* store input in Properties */
                 Input.store(Output,null);
-                for(int CounterA=0;CounterA<6;CounterA +=2) {
-                    System.out.printf("%-15s%03d\n",
-                            Input.getProperty(String.valueOf(CounterA)),
-                            Integer.valueOf(Input.getProperty(String.valueOf(CounterA+1))));
-
-                }
-
-
-
-
-            }catch(Exception e){System.out.println("  "+e );}
-
-
-
-
-
-
-
-
+                for(int CounterA=0;CounterA<6;CounterA +=2)
+                    {
+                        /* read input from Properties and write  */
+                        System.out.printf("%-15s%03d\n",
+                                Input.getProperty(String.valueOf(CounterA)),
+                                Integer.valueOf(Input.getProperty(String.valueOf(CounterA+1))));
+                    }
+            }
+        catch(Exception e)
+        {System.out.println("  "+e );}
 
     }
 }
